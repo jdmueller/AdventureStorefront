060947af589489aa4f48c6345c4a48f1f86def63
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-15 16:56:00 -0500

    powered by pysolution

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -85,11 +85,11 @@ export async function Footer({ channel }: { channel: string }) {
 					<p className="text-sm text-neutral-500">Copyright &copy; {currentYear} Adventure Parts, LLC</p>
 					<p className="flex gap-1 text-sm text-neutral-500">
 						Powered by{" "}
-						<Link target={"_blank"} href={"https://saleor.io/"}>
-							Mueller
+						<Link target={"_blank"} href={"https://pysolution.com/"}>
+							pySolution
 						</Link>{" "}
-						<Link href={"https://github.com/saleor/saleor"} target={"_blank"} className={"opacity-30"}>
-							<Image alt="Saleor github repository" height={20} width={20} src={"/github-mark.svg"} />
+						<Link href={"https://pysolution.com"} target={"_blank"} className={"opacity-30"}>
+							<Image alt="pySolution Python Logo" height={20} width={20} src={"/github-mark.svg"} />
 						</Link>
 					</p>
 				</div>
