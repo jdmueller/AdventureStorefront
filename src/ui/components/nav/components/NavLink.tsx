7874893ab3a3c93721a18257a15ca280eb50ce5c
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-15 20:13:45 -0500

    nav item

--- src/ui/components/nav/components/NavLink.tsx
+++ src/ui/components/nav/components/NavLink.tsx
@@ -14,7 +14,7 @@ export function NavLink({ href, children }: { href: string; children: JSX.Elemen
 			<LinkWithChannel
 				href={href}
 				className={clsx(
-					isActive ? "border-neutral-900 text-neutral-900" : "border-transparent text-neutral-500",
+					isActive ? "border-neutral-900 text-neutral-900" : "border-transparent text-neutral-800",
 					"inline-flex items-center border-b-2 pt-px text-sm font-medium hover:text-neutral-700",
 				)}
 			>
