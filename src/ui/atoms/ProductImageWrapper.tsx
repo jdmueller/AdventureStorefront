ba337fcf1d4642c5a4fa362014b7538931742d77
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-15 20:22:52 -0500

    less orangy

--- src/ui/atoms/ProductImageWrapper.tsx
+++ src/ui/atoms/ProductImageWrapper.tsx
@@ -2,7 +2,7 @@ import NextImage, { type ImageProps } from "next/image";
 
 export const ProductImageWrapper = (props: ImageProps) => {
 	return (
-		<div className="aspect-square overflow-hidden bg-orange-100">
+		<div className="aspect-square overflow-hidden bg-orange-50">
 			<NextImage {...props} className="h-full w-full object-contain object-center p-2" />
 		</div>
 	);
