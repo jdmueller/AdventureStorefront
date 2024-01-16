621dfedf5dd9ec57d2b55774614d60a6a3e089f6
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-15 19:36:48 -0500

    header background

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -3,7 +3,7 @@ import { Nav } from "./nav/Nav";
 
 export function Header({ channel }: { channel: string }) {
 	return (
-		<header className="bg-neutral-300/200 sticky top-0 z-20 backdrop-blur-md">
+		<header className="bg-orange-300/200 sticky top-0 z-20 backdrop-blur-md">
 			<div className="mx-auto max-w-7xl px-3 sm:px-8">
 				<div className="flex h-16 justify-between gap-4 md:gap-8">
 					<Logo />
