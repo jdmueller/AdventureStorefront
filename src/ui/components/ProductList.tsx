f6d73cff3f007119e56ea458991b04271b0577be
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-17 20:21:29 -0500

    product list columns

--- src/ui/components/ProductList.tsx
+++ src/ui/components/ProductList.tsx
@@ -3,7 +3,7 @@ import { type ProductListItemFragment } from "@/gql/graphql";
 
 export const ProductList = ({ products }: { products: readonly ProductListItemFragment[] }) => {
 	return (
-		<ul role="list" data-testid="ProductList" className="lg:grid-cols4 grid grid-cols-1 gap-8 sm:grid-cols-2">
+		<ul role="list" data-testid="ProductList" className="lg:grid-cols4 grid grid-cols-1 gap-8 sm:grid-cols-3">
 			{products.map((product, index) => (
 				<ProductElement
 					key={product.id}
