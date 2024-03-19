0368040bb017fa1bb8186df0a564f173371294b8
Jonathan Mueller <jdmueller@protonmail.ch>
2024-03-19 18:57:26 -0500

    update page titles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { DraftModeNotification } from "@/ui/components/DraftModeNotification";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Saleor Storefront example",
-	description: "Starter pack for building performant e-commerce experiences with Saleor.",
+	title: "Adventure Parts, LLC",
+	description: "igh end performance parts for Adventure Motorcycles and Electric Bikes.",
 	metadataBase: process.env.NEXT_PUBLIC_STOREFRONT_URL
 		? new URL(process.env.NEXT_PUBLIC_STOREFRONT_URL)
 		: undefined,
